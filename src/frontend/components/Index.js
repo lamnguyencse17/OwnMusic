c1d6fc58a6c8642bbea8b2181914fb1bbfd0a34d
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-10-31 10:07:54 +0700

    Config tailwindcss

--- src/frontend/components/Index.js
+++ src/frontend/components/Index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../store";
 import App from "./App";
+import "../assets/main.css";
 
 ReactDOM.render(
   <Provider store={store}>
