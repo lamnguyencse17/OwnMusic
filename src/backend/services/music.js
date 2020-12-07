e7b3f169d1dbab75d2bf9d28487315b77e359111
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-12-07 23:47:37 +0700

    Some Music Service Functions

---
+++ src/backend/services/music.js
@@ -0,0 +1,38 @@
+import musicModel from "../models/musics";
+import mongoose from "mongoose";
+
+export const createMusic = async ({name, description, coverURL, demoURL, downloadURL, price, artist}) => {
+  const result = await musicModel.create({
+    name, description, coverURL, demoURL, downloadURL, price, artist: mongoose.Types.ObjectId(artist)
+  });
+  return { result, status: true };
+};
+
+export const getSingleMusic = async (musicId) => {
+  const result = await musicModel.findOne({ _id: mongoose.Types.ObjectId(musicId) }).select("-downloadURL").populate("artist", "name _id").lean();
+  if (!result){
+    return {status: false, message: "No Music With Such ID Found"};
+  }
+  return {status: true, music: result};
+};
+
+export const getMusicByPage = async ({limit, offset}) => {
+  try {
+    const result = await musicModel.find({}).skip(offset).limit(limit).select("-downloadURL").populate("artist", "name _id").lean();
+    return {status: true, music: result};
+  } catch(err) {
+    console.error(err);
+    return {status: false, message: err};
+  }
+};
+
+export const getMusicSuggestions = async () => {
+  try {
+    const result = await musicModel.aggregate([{$sample: {size: 5}}]);
+    console.log(result);
+    return {status: true, music: result};
+  } catch (err) {
+    console.error(err);
+    return {status: false, message: err};
+  }
+};
