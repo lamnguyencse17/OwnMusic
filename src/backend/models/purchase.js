91894f2477f1a735fe8764278bea4c7af3fa5473
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-12-11 13:21:00 +0700

    Implement CreateTransaction with CPM API and Adjust Transaction Model

--- src/backend/models/purchase.js
+++ src/backend/models/purchase.js
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 const Purchases = mongoose.Schema;
 
 export const purchaseSchema = new Purchases({
-  user: {type: mongoose.Schema.Types.ObjectId, required: true},
-  artist: {type: mongoose.Schema.Types.ObjectId, required: true},
-  musics: {type: mongoose.Schema.Types.ObjectId}
+  user: { type: mongoose.Schema.Types.ObjectId, required: true },
+  artist: { type: mongoose.Schema.Types.ObjectId, required: true },
+  musics: { type: mongoose.Schema.Types.ObjectId },
+  isCompleted: { type: Boolean, required: true },
 });
 
 const purchaseModel = mongoose.model("Purchases", purchaseSchema);
--- src/backend/services/coinpayment.js
+++ src/backend/services/coinpayment.js
@@ -6,3 +6,20 @@ const coinpaymentClient = new Coinpayments({
   key: process.env.CPM_KEY,
   secret: process.env.CPM_SECRET,
 });
+
+export const createTransaction = async ({
+  amount,
+  buyer_email,
+  success_url,
+  cancel_url,
+}) => {
+  const createTransactionResult = await coinpaymentClient.createTransaction({
+    amount,
+    buyer_email,
+    success_url,
+    cancel_url,
+    currency1: "USD",
+    currency2: "LTCT",
+  });
+  return createTransactionResult;
+};
